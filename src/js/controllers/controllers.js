7a0bf98afa9c7a4e82a65426ffab09f5314189f2
Yauhen <penkin.evgeniy@gmail.com>
2017-02-27 21:38:35 +0300

    Try AngularJS with ToDoApp

---
+++ src/js/controllers/controllers.js
@@ -0,0 +1,30 @@
+angular.module('todoApp.controller', [])
+  .controller('todoController', ['$scope', '$filter', function ($scope, $filter) {
+    $scope.tempInput = '';
+    $scope.tasksArray = [];
+    $scope.show = '';
+    $scope.addNew = function () {
+      if ($scope.tempInput.trim()) {
+        $scope.tasksArray.push({value: $scope.tempInput, checked: false, edit: false});
+        $scope.tempInput = '';
+      }
+    };
+    $scope.deleteTask = function (item) {
+      let index = $scope.tasksArray.indexOf(item);
+      $scope.tasksArray.splice(index, 1);
+    };
+    $scope.showFn = function (item) {
+      if ($scope.show === 'All' || $scope.show === '') {
+        return true;
+      } else if (item.checked && $scope.show === 'Completed') {
+        return true;
+      } else if (!item.checked && $scope.show === 'Active') {
+        return true;
+      } else {
+        return false;
+      }
+    };
+    $scope.clearCompleted = function (item) {
+      $scope.tasksArray = $scope.tasksArray.filter(item => !item.checked);
+    };
+  }]);
