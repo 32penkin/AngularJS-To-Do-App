1ee00d19329259b327930b6155e318b3bae71408
Yauhen <penkin.evgeniy@gmail.com>
2017-03-12 15:39:05 +0300

    Sass styles added

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,43 @@
-angular.module('todoApp', [
-  'todoApp.controller'
-]);
+import '../styles/styles.scss';
+import angular from 'angular';
+
+angular.module('todoApp', [])
+  .controller('todoController', ['$scope', function ($scope) {
+    $scope.tempInput = '';
+    $scope.tasksArray = [];
+    $scope.show = '';
+    $scope.addNew = function () {
+      if ($scope.tempInput.trim()) {
+        $scope.tasksArray.push({value: $scope.tempInput, checked: false, edit: false});
+        $scope.tempInput = '';
+      }
+    };
+    $scope.editTask = function (item) {
+      let index = $scope.tasksArray.indexOf(item);
+      $scope.tasksArray[index].edit = !$scope.tasksArray[index].edit;
+    };
+    $scope.deleteTask = function (item) {
+      let index = $scope.tasksArray.indexOf(item);
+      $scope.tasksArray.splice(index, 1);
+    };
+    $scope.showFn = function (item) {
+      if ($scope.show === 'All' || $scope.show === '') {
+        return true;
+      } else if (item.checked && $scope.show === 'Completed') {
+        return true;
+      } else if (!item.checked && $scope.show === 'Active') {
+        return true;
+      } else {
+        return false;
+      }
+    };
+    $scope.clearCompleted = function (item) {
+      $scope.tasksArray = $scope.tasksArray.filter(item => !item.checked);
+      $scope.show = 'All';
+    };
+    $scope.toggleAll = function (allchecked) {
+      $scope.tasksArray.forEach(function (todo) {
+        todo.checked = !todo.checked;
+      })
+    }
+  }]);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: "./src/js/app",
+  output: {
+    path: __dirname + "/dist",
+    filename: "build.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader?presets[]=es2015'
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*$|$)/,
+        loader: 'file-loader'
+      },
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My ToDoList',
+      filename: 'index.html',
+      template: 'index.template.ejs',
+      inject: 'body'
+    })
+  ],
+  node: {
+    fs: 'empty'
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    contentBase: __dirname + '/dist'
+  }
+}
